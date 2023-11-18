85fb800713f2c04993701649929b74df27047aae
Conor O'Brien <conor.obrien24@gmail.com>
2023-11-18 14:43:02 +0000

    pad_with_ellipsis test cases

--- test/tokenizer_test.cc
+++ test/tokenizer_test.cc
@@ -14,3 +14,11 @@ TEST(IsCharInPatternTest, TokenizerAssertions)
     ASSERT_TRUE(RueTokenizer::is_char_in_pattern(' ', "with space"));
     ASSERT_TRUE(RueTokenizer::is_char_in_pattern('\n', "with\nnewline"));
 }
+
+TEST(EllipsisPaddingTest, TokenizerAssertions)
+{
+    ASSERT_EQ("Symb0l$", RueTokenizer::pad_with_ellipsis("Symb0l$", 99));
+    ASSERT_EQ("Rue", RueTokenizer::pad_with_ellipsis("Rue", 3));
+    ASSERT_EQ("Rue", RueTokenizer::pad_with_ellipsis("Rue", 1));
+    ASSERT_EQ("R...", RueTokenizer::pad_with_ellipsis("Rue", 4));
+}
